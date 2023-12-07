61ee425cad609ec05181111eb7fda8a558f0d193
Nahuel Luca <nahuelluca20@gmail.com>
2023-12-07 17:58:35 -0300

    Refactor code and add new dependencies

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,42 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+export async function generateStaticParams() {
+  const files = fs.readdirSync(path.join("blog"));
+
+  const paths = files.map((filename) => ({
+    slug: filename.replace(".mdx", ""),
+  }));
+
+  return paths;
+}
+
+function getPost({ slug }: { slug: string }) {
+  const markdownFile = fs.readFileSync(
+    path.join("blog", slug + ".mdx"),
+    "utf-8"
+  );
+
+  const { data: fontMatter, content } = matter(markdownFile);
+
+  return {
+    fontMatter,
+    slug,
+    content,
+  };
+}
+
+export default function page({ params }: any) {
+  const props = getPost(params);
+
+  return (
+    <article className="prose prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert mx-auto">
+      <h1>{props.fontMatter.title}</h1>
+
+      <MDXRemote source={props.content}></MDXRemote>
+    </article>
+  );
+}
